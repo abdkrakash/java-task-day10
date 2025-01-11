dc74ef63b46cb6f0a2022d55b3a146ce9a13a380
abdkrakash <ABDKARKASH@GMAIL.COM>
2025-01-12 00:17:22 +0300

    done

---
+++ java-task-day10/main.js
@@ -0,0 +1,18 @@
+
+async function newData(){ 
+    let response = await fetch("https://api.github.com/users/abdkrakash/repos", { 
+    method: "GET",
+  });
+  
+  let data = await response.json();
+  
+  for(var i=0; i< data.length; i++){
+    console.log(data[i].name)
+  
+  }
+  }
+   
+  newData();
+
+
+  
